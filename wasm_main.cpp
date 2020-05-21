a3a469e737f67c8982349d3a823f5dc916c4ac5e
Rodrigo Bernardes <rodrigovalente1996@gmail.com>
2020-05-21 20:14:54 -0300

    Testing with unique ptr

--- wasm_main.cpp
+++ wasm_main.cpp
@@ -3,20 +3,21 @@
 
 #include "src/graphics/sdl2_handler.h"
 #include <emscripten.h>
+#include <memory>
 #include "src/chip8.cpp"
 
-SDL2_handler<chip8::Core> h;
+std::unique_ptr<SDL2_handler<chip8::Core>> h = std::make_unique< SDL2_handler<chip8::Core> >();
 
 
 void wasm_loop()
 {
-    h.loop();
+    h->loop();
 }
 
 int main(int argc, char ** argv )
 {
-    h.get_emu()->load_ROM("chip8Pic.ch8");
+    h->get_emu()->load_ROM("chip8Pic.ch8");
 
-    emscripten_set_main_loop(&wasm_loop,-1,1);
+    emscripten_set_main_loop(&wasm_loop,60,1);
 
 }
